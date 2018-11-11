57a0ac2288c253023f450f761c262e13d164c1ac
jiangqianghua <240437339@qq.cvom>
2018-11-11 11:39:49 +0800

    完成项目最初始化的构建

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,1 +1,11 @@
-index.js
+import React from 'react';
+import ReactDom from 'react-dom'
+
+import {Provider} from 'react-redux';
+
+import Main from './Main/main.jsx';
+
+ReactDom.render(
+		<Provider store={store}><Main /></Provider>,
+		document.getElementById('root')
+	);
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,19 +1,38 @@
  const path = require('path');
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
  const srcRoot = path.resolve(__dirname,'src');
  const devPath = path.resolve(__dirname,'dev');
  const pageDir = path.resolve(srcRoot,'page');
  const mainFile = 'index.js'
 const fs = require('fs');
 
+function getHtmlArray(entryMap){
+    let htmlArray = [];
+
+    Object.keys(entryMap).forEach((key)=>{
+        let fullPathName = path.resolve(pageDir,key);
+        let fileName = path.resolve(fullPathName, key+'.html');
+        console.log("htmlArray:" + fileName);
+        if(fs.existsSync(fileName)){
+            htmlArray.push(new HtmlWebpackPlugin({
+                filename:key+'.html',
+                template:fileName,
+                chunks:[key]  //引入js文件
+            }));
+        }
+    });
+    return htmlArray ;
+}
+
 function getEntry(){
     let entryMap = {};
 
     fs.readdirSync(pageDir).forEach((pathname)=>{
         let fullPathName = path.resolve(pageDir, pathname);
-        console.log("fullPathName="+fullPathName);
+       // console.log("fullPathName="+fullPathName);
         let stat = fs.statSync(fullPathName);
         let fileName = path.resolve(fullPathName, mainFile);
-        console.log("fileName="+fileName);
+        console.log("getEntry:"+fileName);
         if (stat.isDirectory() && fs.existsSync(fileName)) {
             entryMap[pathname] = fileName;
         }
@@ -24,6 +43,7 @@ function getEntry(){
 }
 
 const entryMap = getEntry();
+const htmlArray = getHtmlArray(entryMap);
  module.exports={
  	mode:'development',
  	entry:entryMap,
@@ -33,9 +53,13 @@ const entryMap = getEntry();
  	},
  	module:{
  		rules:[
+            { test: /\.(js|jsx)$/, use: [{loader:'babel-loader'}] , include:srcRoot},
  			{ test: /\.css$/ , use:['style-loader' , 'css-loader'] , include:srcRoot },
  			{ test: /\.scss$/ , use:['style-loader' , 'css-loader','sass-loader'] , include:srcRoot},
  			{ test: /\.(png|jpg|jpeg)$/, use:'url-loader?limit=8192', include:srcRoot}
  		]
- 	}
+ 	},
+    plugins:[
+        
+    ].concat(htmlArray)
  }
